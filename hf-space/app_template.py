edfcde278ddc15f82309535ab189c12a062dcd22
Chansung Park <deep.diver.csp@gmail.com>
2022-05-28 14:36:33 +0900

    Update app_template.py

--- hf-space/app_template.py
+++ hf-space/app_template.py
@@ -1,3 +1,4 @@
+from os import walk
 import tarfile
 import gradio as gr
 from huggingface_hub import hf_hub_download
@@ -6,14 +7,14 @@ from tensorflow.keras.applications import resnet50
 
 def load_model(tar_file: str='outputs/model.tar.gz'):
     tar_file = tarfile.open(tar_file)
-    tar_file.extractall('./')
+    tar_file.extractall('.')
     tar_file.close()
 
     model_path = 'model'
     return keras.models.load_model(model_path)
 
-hf_hub_download(repo_id='$MODEL_REPO_ID', filename='outputs/model.tar.gz')
-model = load_model()
+filepath = hf_hub_download(repo_id='$MODEL_REPO_ID', filename='outputs/model.tar.gz', force_filename='model.tar.gz')
+model = load_model(tar_file=filepath)
 labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 def classify_image(inp):
@@ -28,3 +29,4 @@ iface = gr.Interface(fn=classify_image,
              inputs=gr.inputs.Image(shape=(224, 224)),
              outputs=gr.outputs.Label(num_top_classes=3)).launch()
 iface.launch()
+
